51a607d9918e2cbff8c9a707ef2be5df3df6b2b4
SkylerElmstrom <skylerelmstrom@users.noreply.github.com>
2021-01-26 13:45:59 -0800

    Publishing Distill Blog for Daily R Challenge

---
+++ GettingStarted.r
@@ -0,0 +1,14 @@
+
+# To start using Distill, install/load the library
+library(distill)
+
+# Create a Post, saves to _posts folder under project directory
+create_post("Name of Post")
+create_post("Name of Post", draft = TRUE)
+
+
+create_post("Day 1 - ggplot2")
+create_post("Day 2 - dplyr")
+create_post("Day 3 - plyr")
+create_post("Day 4 - data.table")
+
